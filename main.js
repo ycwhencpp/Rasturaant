1577742cbd1dd531ff55db91a02f75293a33eba2
ycwhencpp <anurag.jay1002@gmail.com>
2021-08-13 00:29:45 -0700

    made home section resposive

--- main.js
+++ main.js
@@ -9,18 +9,30 @@ window.addEventListener("scroll",()=>{
 //for clearing contact form on submit
 
 const submitform =document.querySelector('input[type="submit"]');
+
 submitform.addEventListener("click",()=>{
         document.querySelector("textarea").value=null;
 });
 
 //for making navlink active on click
 
-const navlink = document.querySelectorAll(".navlinks li a");
+const navlink_a = document.querySelectorAll(".navlinks li a");
 
 function addclass(){
-    navlink.forEach(n=>n.classList.remove("active"));
+    navlink_a.forEach(n=>n.classList.remove("active"));
     this.classList.add("active");
     }
-navlink.forEach(n=>n.addEventListener("click",addclass));
- 
+navlink_a.forEach(n=>n.addEventListener("click",addclass));
+
+
+ // for menutoggle
+const menutoggle= document.querySelector(".menu-toggle");
+
+menutoggle.addEventListener("click",()=>{
+   menutoggle.classList.toggle("active");
+
+   document.querySelector(".navlinks").classList.toggle("show");
+    
+   
+});
 
